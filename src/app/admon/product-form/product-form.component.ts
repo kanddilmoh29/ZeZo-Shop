402c9d1da8305f4288c1d84e49dc1c6836585061
Faris Kanddil <kanddilmoh29@gmail.com>
2018-10-16 19:51:16 -0400

    push product to firebase

--- src/app/admon/product-form/product-form.component.ts
+++ src/app/admon/product-form/product-form.component.ts
@@ -1,6 +1,8 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { ProductService } from './../../product.service';
 import { CategoriesService } from './../../categories.service';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -9,14 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product ;
 
-  constructor(categoriesService: CategoriesService, private productService: ProductService) { 
+  constructor(
+    private categoriesService: CategoriesService, 
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { 
+      
     this.categories$ = categoriesService.getCategories();
+
+    let id = this.route.snapshot.paramMap.get('id');
+    if (id) this.productService.getProducts(id).take(1).subscribe(p => this.product = p);
   }
 
   save(product) {
     this.productService.create(product);
-    console.log(product);
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,13 +70,18 @@ import { FormsModule } from '@angular/forms';
         canActivate: [AuthGuard] 
       },
       { 
-        path: 'admin/products', 
-        component: AdminProductsComponent, 
+        path: 'admin/products/new', 
+        component: ProductFormComponent, 
         canActivate: [AuthGuard] 
       },
       { 
-        path: 'admin/products/new', 
+        path: 'admin/products/:id', 
         component: ProductFormComponent, 
+        canActivate: [AuthGuard] 
+      },
+      { 
+        path: 'admin/products', 
+        component: AdminProductsComponent, 
         canActivate: [AuthGuard] 
       },
       { 
